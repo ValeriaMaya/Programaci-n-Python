e595ea0fcb290e7f7a443f1ee1bdfa71762eb027
ValeriaMaya <evmontsemaya@gmail.com>
2016-11-29 15:19:42 -0600

    Add files via upload

---
+++ Laboratorio 3/ejercicio1.py
@@ -0,0 +1,8 @@
+import math
+def dist(lista):
+  t1=(lista[0][0])*math.pi/180
+  t2=(lista[1][0])*math.pi/180
+  g1=(lista[0][1])*math.pi/180
+  g2=(lista[1][1])*math.pi/180
+  d=6371.01*math.acos(math.sin(t1)*math.sin(t2)+math.cos(t1)*math.cos(t2)*math.cos(g1-g2))
+  print d,"kilometros"
---
+++ Laboratorio 3/ejercicio2.py
@@ -0,0 +1,10 @@
+import math
+def sum(x):
+  d1=x/1000
+  x2=x%1000
+  d2=x2/100
+  x3=x2%100
+  d3=x3/10
+  x4=x3%10
+  sum=d1+d2+d3+x4
+  print sum
