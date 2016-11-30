3ce21db2bd365aa82b0220aa7865a48ba7ec6d87
ValeriaMaya <evmontsemaya@gmail.com>
2016-11-29 18:29:10 -0600

    Add files via upload

---
+++ Laboratorio 3/ejercicio3.py
@@ -0,0 +1,20 @@
+print "Dame tres número enteros y los ordenaré de menor a mayor"
+x1=int(input("1er Num"))
+x2=int(input("2do Num"))
+x3=int(input("3er Num"))
+if x1>x2:
+  if x2>x3:
+    print x3, x2, x1
+  else:
+    if x1>x3:
+      print x2, x3, x1
+    else:
+      print x2, x1, x3
+else:
+  if x1>x3:
+    print x3, x1, x2
+  else:
+    if x2>x3:
+      print x1, x3, x2
+    else:
+      print x1, x2, x3
