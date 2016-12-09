e682587505958099be051a65e94b059a64cc9414
ValeriaMaya <evmontsemaya@gmail.com>
2016-12-09 02:08:50 -0600

    lab3b

--- Laboratorio 3B/Ejercicio 1/ejercicio1a.py
+++ Laboratorio 3B/Ejercicio 1/ejercicio1a.py
@@ -1,10 +1,13 @@
+#Ejercicio1
+#Valeria Montserrat Maya Gonzalez
+
 import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(-6,2,100)
 y=5-(4*x)-x**2
 plt.plot(x,y,linewidth=3,color='orange')
 plt.legend()
-plt.title('f(x)=5-4x-x^2')
+plt.title('$f(x)=5-4x-x^2$')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
