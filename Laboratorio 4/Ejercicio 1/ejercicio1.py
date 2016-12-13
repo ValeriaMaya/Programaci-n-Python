448711555c310de81915d1be1dbe1c4dd3b53271
ValeriaMaya <evmontsemaya@gmail.com>
2016-12-13 00:13:49 -0600

    Mejoras

--- Laboratorio 4/Ejercicio 1/ejercicio1.py
+++ Laboratorio 4/Ejercicio 1/ejercicio1.py
@@ -1,3 +1,8 @@
+#Ejercicio1
+#Valeria Montserrat Maya Gonzalez
+#Función que muestra en la pantalla las n líneas de un archivo
+#Para ello se debe introducir el nombre o ruta del archivo entre comillas, y el número de líneas que se quieran leer
+
 def leertxt(s,n):
     archivo=open(s,"r")
     for linea in range(1,(n+1)):
