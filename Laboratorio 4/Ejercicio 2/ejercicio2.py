37a39348044790e3782a631cc0383154ba975d1b
ValeriaMaya <evmontsemaya@gmail.com>
2016-12-13 01:29:53 -0600

    Mejoras

--- Laboratorio 4/Ejercicio 2/ejercicio2.py
+++ Laboratorio 4/Ejercicio 2/ejercicio2.py
@@ -1,7 +1,7 @@
 #Ejercicio2
 #Valeria Montserrat Maya Gonzalez
 #Función que concatena archivos y los devuelve como un archivo nuevo cuyo nombre debe indicarse al inicio
-
+#Para ello debe introducirse una lista [], cuyos elementos sean las rutas o nombres de archivos entre comillas y, el nombre, también entre comillas, qu se quiere dar al nuevo archivo al concatenar los anteriores.
 def concatenar(n,m):
     nvo=open(m,"a")
     for i in n:
