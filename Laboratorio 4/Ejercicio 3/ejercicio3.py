bb20adeeffeafed6eec1dfd690878876f6aa1207
ValeriaMaya <fismat@iMac-7.local>
2016-12-16 11:27:26 -0600

    ejercicio3 terminado

--- Laboratorio 4/Ejercicio 3/ejercicio3.py
+++ Laboratorio 4/Ejercicio 3/ejercicio3.py
@@ -1,12 +1,20 @@
 #Ejercicio3
 #Valeria Montserrat Maya Gonzalez
-#Numerar las líneas de un archivo de texto ya existente, guardarlo como un archivo independiente cuyo nombre sea especificado al inicio.
+#Numerar las lineas de un archivo de texto ya existente, guardarlo como un archivo independiente cuyo nombre sea especificado al inicio.
+
+print "Numeraremos las lineas de su archivo"
+n=raw_input("Nombre del archivo de entrada (con extension)")
+m=raw_input("Nombre de su archivo de salida (con extension)")
 
 def numerar(n,m):
     v=open(n,"r")
-    nvo=open(m,"w")
-    nLines=0
-    for currLine in v:
-        nLines=nLines+1
-        for linea in v:
-            nvo.write(nLines+linea+"\n")
+    lineas=[]
+    lineas=v.readlines()
+    nvo=open(m,"w+")
+    for i in range(1,len(lineas)):
+        nvo.write(str(i)+")"+lineas[i])
+    print "El archivo esta listo en el direcotrio"
+
+numerar(n,m)
+v.close()
+nvo.close()
