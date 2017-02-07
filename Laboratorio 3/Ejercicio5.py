b25e59448afe2286621e0f011764a9dd6874074e
ValeriaMaya <evmontsemaya@gmail.com>
2017-02-07 02:07:41 -0600

    Create Ejercicio5.py

---
+++ Laboratorio 3/Ejercicio5.py
@@ -0,0 +1,12 @@
+#Ejercicio5
+#Valeria Montserrat Maya Gonzalez
+nombre=raw_input('Ingrese su nombre')
+s=map(str,nombre)
+v=('a','e','i','o','u','A','E','I','O','U')
+if nombre[0]==v[0] or nombre[0]==v[1] or nombre[0]==v[2] or nombre[0]==v[3] or nombre[0]==v[4]:
+    print "La palabra empieza por vocal"
+else:
+    if nombre[0]==v[5] or nombre[0]==v[6] or nombre[0]==v[7] or nombre[0]==v[8] or nonmbre[0]==v[9]:
+        print "La palabra empieza por vocal"
+    else:
+        print "La palabra no empieza por vocal"
